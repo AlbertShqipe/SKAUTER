6596a8882f46214f1c3fc8af70d58afbb212a41d
Albert Nikolli <albert_nikolli@icloud.com>
2026-01-27 20:54:52 +0100

    took care of few view issues

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,13 +1,14 @@
 class RegionsController < ApplicationController
+  def index
+    @counties = County
+      .joins(:locations)
+      .where(locations: { available: true })
+      .distinct
+      .order(:name)
+  end
+
   def show
     @county = County.find_by!(slug: params[:slug])
     @locations = @county.locations.where(available: true)
-  end
-
-  def index
-    @regions = Location.all
-    @distinct_cities = Location.where(available: true).distinct.pluck(:city).sort
-    @distinct_counties = Location.where(available: true).distinct.pluck(:county.name).sort
-
   end
 end
