2036027d1db4c65c5ff5b8182742844349d63ee9
Albert Nikolli <albert_nikolli@icloud.com>
2026-03-07 23:32:34 +0100

    working the global overlay

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
+  connect() {
+    this.onScroll() // Check scroll position on load
+  }
+
   onScroll() {
     if (window.scrollY > 20) {
       this.element.classList.add("scrolled")
